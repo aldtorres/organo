ca1c760f8f5302fee3efd2d8a517792b6c18e934
torrestor <torres.jovem@gmail.com>
2025-11-13 11:44:26 -0300

    ngClass -com- CommonModule

--- src/app/components/genero-literario/genero-literario.component.ts
+++ src/app/components/genero-literario/genero-literario.component.ts
@@ -2,10 +2,11 @@ import { Component, input } from '@angular/core';
 import { LivroComponent } from "../livro/livro.component";
 import { IGeneroLiterario, ILivro } from '../livro/ILivro';
 import { livros } from '../../mock-livros';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-genero-literario',
-  imports: [LivroComponent],
+  imports: [LivroComponent, CommonModule],
   templateUrl: './genero-literario.component.html',
   styleUrl: './genero-literario.component.css'
 })
